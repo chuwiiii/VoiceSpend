0cfa9635834fda593fea267e5ac3bee36486b603
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 00:01:17 -0800

    add
    app.js

--- voicespend/src/App.js
+++ voicespend/src/App.js
@@ -1,2 +1,7 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+
+import Details from './components/Details/Details';
+import Main from './components/Main/Main';
+
+import useStyles from './appStyles';
