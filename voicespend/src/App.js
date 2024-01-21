9d5f675654cd6495b68a5155592b4ee3d1addc85
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 00:08:28 -0800

    add
    app.js

--- voicespend/src/App.js
+++ voicespend/src/App.js
@@ -14,3 +14,15 @@ const App = () => {
             <Grid item xs={12} sm={4}>
                 <Details title="Income"/>
             </Grid>
+            <Grid item xs={12} sm={3}>
+                <Main />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+                <Details title="Expenses"/>
+            </Grid>
+        </Grid>
+    </div>
+  );
+}
+
+export default App
