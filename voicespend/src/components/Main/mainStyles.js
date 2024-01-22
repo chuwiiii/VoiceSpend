af17d43ac6f2bb7bb1a6f4af0506140c9f1c6823
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 22:48:37 -0800

    add
    mainStyles.js

--- voicespend/src/components/Main/mainStyles.js
+++ voicespend/src/components/Main/mainStyles.js
@@ -12,3 +12,6 @@ export default makeStyles((theme) => ({
             duration: theme.transitions.duration.shortest,
           }),
         },
+        expandOpen: {
+            transform: 'rotate(180deg)',
+          },
