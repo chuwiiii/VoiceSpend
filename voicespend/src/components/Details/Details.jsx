e7ca7a707ff64577e49e7281aea0b831358b1ca0
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 21:36:24 -0800

    add
    Details.jsx

--- voicespend/src/components/Details/Details.jsx
+++ voicespend/src/components/Details/Details.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
+
+import useStyles from './styles'
