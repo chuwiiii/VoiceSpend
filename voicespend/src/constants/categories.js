79df8a6d171c445b7be9c6566c9e18ea8727d047
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-22 21:31:02 +0800

    Update
    categories.js

--- voicespend/src/constants/categories.js
+++ voicespend/src/constants/categories.js
@@ -27,4 +27,7 @@ export const incomeCategories = [
     { type: 'Other', amount: 0, color: expenseColors[10] },
   ];
 
-  
+  export const resetCategories = () => {
+    incomeCategories.forEach((c) => c.amount = 0);
+    expenseCategories.forEach((c) => c.amount = 0);
+  };
