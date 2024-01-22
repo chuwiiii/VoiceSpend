18bff2c9da79af5c4090f7a5f433155b1aca57c8
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-22 21:16:18 +0800

    Update
    context.js

--- voicespend/src/context/context.js
+++ voicespend/src/context/context.js
@@ -14,14 +14,13 @@ export const Provider = ({ children }) => {
     dispatch({ type: 'DELETE_TRANSACTION', payload: id });
   const addTransaction = (transaction) =>
     dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
-  const clearTransactions = () =>
-    dispatch({ type: 'CLEAR_TRANSACTIONS' });
+  
 
   const balance = useMemo(() =>
     transactions.reduce((acc, currVal) => currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount, 0),
     [transactions]
   );
-  
+
     return(
         <ExpenseTrackerContext.Provider value={{appName: 'Expense Tracker'}}>
             {children}
