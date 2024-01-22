52508b5f00181bb0fc2322d40855a77e8188de45
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 22:27:07 -0800

    add
    Main.jsx

--- voicespend/src/components/Main/Main.jsx
+++ voicespend/src/components/Main/Main.jsx
@@ -2,3 +2,4 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './mainStyles';
+import Form from './Form/Form';
