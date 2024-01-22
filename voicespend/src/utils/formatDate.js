e91f8553421ac0eac159853a12bbfabf7f1e0f60
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-22 21:38:40 +0800

    Update
    formatDate.js

--- voicespend/src/utils/formatDate.js
+++ voicespend/src/utils/formatDate.js
@@ -1,4 +1,7 @@
 const formatDate = (date) => {
-    
+    const d = new Date(date);
+    let month = `${d.getMonth() + 1}`;
+    let day = `${d.getDate()}`; 
+    const year = d.getFullYear();
 
 };
