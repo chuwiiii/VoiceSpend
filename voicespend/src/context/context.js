9dc1e24dd05b00ca3cb6fa2324311cab23c98e53
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-22 21:15:54 +0800

    Update
    context.js

--- voicespend/src/context/context.js
+++ voicespend/src/context/context.js
@@ -1,10 +1,27 @@
 import React, { useReducer, createContext, useMemo } from 'react';
 
+import contextReducer from './contextReducer';
+
 const initialState = [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
+
+    const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+  const deleteTransaction = (id) =>
+    dispatch({ type: 'DELETE_TRANSACTION', payload: id });
+  const addTransaction = (transaction) =>
+    dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+  const clearTransactions = () =>
+    dispatch({ type: 'CLEAR_TRANSACTIONS' });
+
+  const balance = useMemo(() =>
+    transactions.reduce((acc, currVal) => currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount, 0),
+    [transactions]
+  );
+  
     return(
         <ExpenseTrackerContext.Provider value={{appName: 'Expense Tracker'}}>
             {children}
