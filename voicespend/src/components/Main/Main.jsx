fe9e57b16830e3439940fa82ea44ba45e4e0e867
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 22:35:16 -0800

    add
    Main.jsx

--- voicespend/src/components/Main/Main.jsx
+++ voicespend/src/components/Main/Main.jsx
@@ -16,3 +16,18 @@ const Main = () => {
                 {/* info card */}
                 Try saying: Add income for $100 in category salary for Monday.....
             </Typography>
+            <Divider />
+            <Form />
+        </CardContent>
+        <CardContent className={classes.cardContent}>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <List />
+                </Grid>
+            </Grid>
+        </CardContent>
+    </Card>
+  );
+}
+
+export default Main
