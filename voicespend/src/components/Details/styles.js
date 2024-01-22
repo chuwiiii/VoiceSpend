98f10d4b474aa44dfc00a6245a31ae9dbbc72f8e
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-21 21:45:40 -0800

    add
    styles.js

--- voicespend/src/components/Details/styles.js
+++ voicespend/src/components/Details/styles.js
@@ -4,3 +4,7 @@ export default makeStyles(() =>({
     income: {
         borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
       },
+      expense: {
+        borderBottom: '10px solid rgba(255, 0, 0, 0.5)',
+      },
+}));
