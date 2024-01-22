683f7eea8fa645cf49c81e199d3c5f876dd40fdf
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-22 21:30:29 +0800

    Update
    categories.js

--- voicespend/src/constants/categories.js
+++ voicespend/src/constants/categories.js
@@ -12,3 +12,19 @@ export const incomeCategories = [
     { type: 'Savings', amount: 0, color: incomeColors[7] },
     { type: 'Rental income', amount: 0, color: incomeColors[8] },
   ];
+
+  export const expenseCategories = [
+    { type: 'Bills', amount: 0, color: expenseColors[0] },
+    { type: 'Car', amount: 0, color: expenseColors[1] },
+    { type: 'Clothes', amount: 0, color: expenseColors[2] },
+    { type: 'Travel', amount: 0, color: expenseColors[3] },
+    { type: 'Food', amount: 0, color: expenseColors[4] },
+    { type: 'Shopping', amount: 0, color: expenseColors[5] },
+    { type: 'House', amount: 0, color: expenseColors[6] },
+    { type: 'Entertainment', amount: 0, color: expenseColors[7] },
+    { type: 'Phone', amount: 0, color: expenseColors[8] },
+    { type: 'Pets', amount: 0, color: expenseColors[9] },
+    { type: 'Other', amount: 0, color: expenseColors[10] },
+  ];
+
+  
