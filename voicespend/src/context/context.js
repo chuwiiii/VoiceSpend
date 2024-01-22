bf65c3b57da2afe14a149ee464c07862248997ce
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-22 21:17:05 +0800

    Update
    context.js

--- voicespend/src/context/context.js
+++ voicespend/src/context/context.js
@@ -1,30 +1,34 @@
-import React, { useReducer, createContext, useMemo } from 'react';
+import React, { useReducer, createContext, useMemo } from "react";
 
-import contextReducer from './contextReducer';
+import contextReducer from "./contextReducer";
 
 const initialState = [];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
+  const [transactions, dispatch] = useReducer(contextReducer, initialState);
 
-    const [transactions, dispatch] = useReducer(contextReducer, initialState);
-
-  const deleteTransaction = (id) =>
-    dispatch({ type: 'DELETE_TRANSACTION', payload: id });
-  const addTransaction = (transaction) =>
-    dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
   
+  const addTransaction = (transaction) =>
+    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+  const clearTransactions = () => dispatch({ type: "CLEAR_TRANSACTIONS" });
 
-  const balance = useMemo(() =>
-    transactions.reduce((acc, currVal) => currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount, 0),
+  const balance = useMemo(
+    () =>
+      transactions.reduce(
+        (acc, currVal) =>
+          currVal.type === "Expense"
+            ? acc - currVal.amount
+            : acc + currVal.amount,
+        0
+      ),
     [transactions]
   );
 
-    return(
-        <ExpenseTrackerContext.Provider value={{appName: 'Expense Tracker'}}>
-            {children}
-        </ExpenseTrackerContext.Provider>
-    )
-
-}
+  return (
+    <ExpenseTrackerContext.Provider value={{ appName: "Expense Tracker" }}>
+      {children}
+    </ExpenseTrackerContext.Provider>
+  );
+};
