af809bacd18cd7073c6943ff0e201f6e31a8a657
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 21:08:09 +0800

    Update
    context.js

--- voicespend/src/context/context.js
+++ voicespend/src/context/context.js
@@ -1,6 +1,6 @@
-import React, { useReducer, createContext, useMemo } from "react";
+import React, { useReducer, createContext, useMemo } from 'react';
 
-import contextReducer from "./contextReducer";
+import contextReducer from './contextReducer';
 
 const initialState = [];
 
@@ -10,20 +10,14 @@ export const Provider = ({ children }) => {
   const [transactions, dispatch] = useReducer(contextReducer, initialState);
 
   const deleteTransaction = (id) =>
-    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+    dispatch({ type: 'DELETE_TRANSACTION', payload: id });
   const addTransaction = (transaction) =>
-    dispatch({ type: "ADD_TRANSACTION", payload: transaction });
-  const clearTransactions = () => dispatch({ type: "CLEAR_TRANSACTIONS" });
+    dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
+  const clearTransactions = () =>
+    dispatch({ type: 'CLEAR_TRANSACTIONS' });
 
-  const balance = useMemo(
-    () =>
-      transactions.reduce(
-        (acc, currVal) =>
-          currVal.type === "Expense"
-            ? acc - currVal.amount
-            : acc + currVal.amount,
-        0
-      ),
+  const balance = useMemo(() =>
+    transactions.reduce((acc, currVal) => currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount, 0),
     [transactions]
   );
 
