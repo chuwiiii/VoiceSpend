bd6a6ded37356a9b2b920bcf8e9793ac524d3260
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 20:47:04 +0800

    Update
    useTransactions.js

--- voicespend/src/useTransactions.js
+++ voicespend/src/useTransactions.js
@@ -6,7 +6,9 @@ import { incomeCategories, expenseCategories, resetCategories } from './constant
 const useTransactions = (title) => {
     resetCategories();
     const { transactions } = useContext(ExpenseTrackerContext);
-  const rightTransactions = transactions.filter((t) => t.type === title);
+    const rightTransactions = transactions.filter((t) => t.type === title);
+    const total = rightTransactions.reduce((acc, currVal) => acc += currVal.amount, 0);
+  const categories = title === 'Income' ? incomeCategories : expenseCategories;
 }
 
 export default useTransactions;
