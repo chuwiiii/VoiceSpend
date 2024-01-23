1ef5b48b1ebebab46dfe56235575c3a4d6426ba2
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 19:28:00 +0800

    Update
    snackbar.jsx

--- voicespend/src/components/Snackbar/snackbar.jsx
+++ voicespend/src/components/Snackbar/snackbar.jsx
@@ -5,6 +5,14 @@ import MuiAlert from '@material-ui/lab/Alert';
 import useStyles from './snackbarStyles';
 
 const CustomizedSnackbar = ({ open, setOpen }) => {
+    const classes = useStyles();
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
     
+        setOpen(false);
+      };
 
 };
