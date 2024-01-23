464dd27f45714e84905dd5f21b4282c398f854be
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 20:43:49 +0800

    Update
    useTransactions.js

--- voicespend/src/useTransactions.js
+++ voicespend/src/useTransactions.js
@@ -1,2 +1,4 @@
 import { useContext } from 'react';
 import { ExpenseTrackerContext } from './context/context';
+
+import { incomeCategories, expenseCategories, resetCategories } from './constants/categories';
