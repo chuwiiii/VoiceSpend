26ff00cadaf58c30399b1f6bdcd93353b5e42126
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 20:20:00 +0800

    Update
    Main.jsx

--- voicespend/src/components/Main/Main.jsx
+++ voicespend/src/components/Main/Main.jsx
@@ -1,36 +1,55 @@
-import React from 'react';
-import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
-import { ExpenseTrackerContext } from '../../context/context';
+import React from "react";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  Typography,
+  Grid,
+  Divider,
+} from "@material-ui/core";
+import { ExpenseTrackerContext } from "../../context/context";
 
-import useStyles from './mainStyles';
-import Form from './Form/Form';
-import List from './List/List';
+import useStyles from "./mainStyles";
+import Form from "./Form/Form";
+import List from "./List/List";
 
 const Main = () => {
-    const classes = useStyles();
-    const { balance } = useContext(ExpenseTrackerContext);
-    
-    return (
-        <Card className={classes.root}>
-            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
-            <CardContent>
-            <Typography align="center" variant="h5"> Total balance: $100</Typography>
-            <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
-                {/* info card */}
-                Try saying: Add income for $100 in category salary for Monday.....
-            </Typography>
-            <Divider />
-            <Form />
-        </CardContent>
-        <CardContent className={classes.cardContent}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <List />
-                </Grid>
-            </Grid>
-        </CardContent>
+  const classes = useStyles();
+  const { balance } = useContext(ExpenseTrackerContext);
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        title={
+          <Typography variant="h4" className={classes.cardHeaderTitle}>
+            VoiceSpend
+          </Typography>
+        }
+      />
+      <CardContent>
+        <Typography align="center" variant="h5">
+          {" "}
+          Total balance: $100
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          style={{ lineHeight: "1.5em", marginTop: "20px" }}
+        >
+          {/* info card */}
+          Try saying: Add income for $100 in category salary for Monday.....
+        </Typography>
+        <Divider />
+        <Form />
+      </CardContent>
+      <CardContent className={classes.cardContent}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <List />
+          </Grid>
+        </Grid>
+      </CardContent>
     </Card>
   );
-}
+};
 
-export default Main
+export default Main;
