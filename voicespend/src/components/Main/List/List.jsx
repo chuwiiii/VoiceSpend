ac68328954467b533bd3046ca76de90ae1e994f3
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-22 18:54:31 -0800

    add
    List.jsx

--- voicespend/src/components/Main/List/List.jsx
+++ voicespend/src/components/Main/List/List.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
+
+import useStyles from './listStyles';
