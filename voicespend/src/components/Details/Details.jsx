49b434041ca29d151bead1cb0ce3b81cf7c4db92
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 19:46:33 +0800

    Update
    Details.jsx

--- voicespend/src/components/Details/Details.jsx
+++ voicespend/src/components/Details/Details.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import useTransactions from '../../useTransactions';
+import useStyles from './stylesDetails';
 
 import useStyles from './styles'
 
