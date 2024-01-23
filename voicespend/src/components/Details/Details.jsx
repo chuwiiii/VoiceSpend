4303150f52d647ebd9d4e85c86ebb735b146096a
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 19:46:59 +0800

    Update
    Details.jsx

--- voicespend/src/components/Details/Details.jsx
+++ voicespend/src/components/Details/Details.jsx
@@ -7,6 +7,8 @@ import useStyles from './styles'
 
 const Details = ({ title }) => {
     const classes = useStyles();
+    const { total } = useTransactions(title);
+    
     return (
         <Card className={ title === 'Income' ? classes.income : classes.expense}>
             <CardHeader title={title} />
