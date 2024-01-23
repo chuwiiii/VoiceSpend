8520dca2ee21000b04c29ef7dd784bed297dfd50
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 21:54:05 +0800

    Update
    Details.jsx

--- voicespend/src/components/Details/Details.jsx
+++ voicespend/src/components/Details/Details.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import useTransactions from '../../useTransactions';
 import useStyles from './detailsStyles';
 
+
+
 const Details = ({ title }) => {
   const classes = useStyles();
   const { total } = useTransactions(title);
+
+  useEffect(() => {
+    document.title = `VoiceSpend`;
+  }, []);
+
 
   return (
     <Card className={title === 'Income' ? classes.income : classes.expense}>
