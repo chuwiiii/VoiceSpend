f055f4d0343045a296aa565707287c9cc9feb485
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 20:38:05 +0800

    Update
    List.jsx

--- voicespend/src/components/Main/List/List.jsx
+++ voicespend/src/components/Main/List/List.jsx
@@ -1,4 +1,4 @@
-import React, { useContex } from 'react';
+import React, { useContext} from 'react';
 import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
 import { Delete, MoneyOff } from '@material-ui/icons';
 
@@ -6,22 +6,22 @@ import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from './listStyles';
 
 const List = () => {
-    const classes = useStyles();
-    const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
+  const classes = useStyles();
+  const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
 
   return (
     <MUIList dense={false} className={classes.list}>
       {transactions.map((transaction) => (
-         <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
-         <ListItem>
-         <ListItemAvatar>
+        <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+            <ListItem>
+              <ListItemAvatar>
                 <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
                   <MoneyOff />
                 </Avatar>
-                </ListItemAvatar>
+              </ListItemAvatar>
               <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`}/>
               <ListItemSecondaryAction>
-                <IconButton edge="end" aria-label="delete" onClick="">
+                <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id)}>
                   <Delete />
                 </IconButton>
               </ListItemSecondaryAction>
