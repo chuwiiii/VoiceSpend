d0724a98bd010c4af0e7876dc04fe3ef3ba53db8
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-23 21:08:51 +0800

    Update
    contextReducer.js

--- voicespend/src/context/contextReducer.js
+++ voicespend/src/context/contextReducer.js
@@ -1,21 +1,21 @@
 const contextReducer = (state, action) => {
-    let transactions;
-  
-    switch (action.type) {
-      case 'DELETE_TRANSACTION':
-        transactions = state.filter((transaction) => transaction.id !== action.payload);
-        return transactions;
+  let transactions;
 
-        case 'ADD_TRANSACTION':
-        transactions = [action.payload, ...state];
-        return transactions;
-  
-      case 'CLEAR_TRANSACTIONS':
-        return [];
+  switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      transactions = state.filter((transaction) => transaction.id !== action.payload);
+      return transactions;
 
-        default:
-            return state;
-        }
-      };
-      
-      export default contextReducer;
+    case 'ADD_TRANSACTION':
+      transactions = [action.payload, ...state];
+      return transactions;
+
+    case 'CLEAR_TRANSACTIONS':
+      return [];
+
+    default:
+      return state;
+  }
+};
+
+export default contextReducer;
