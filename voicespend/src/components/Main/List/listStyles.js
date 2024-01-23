7d6d5e3c2a70778bf874338add9dcd7480a49192
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-22 20:23:59 -0800

    add
    listStyles.js

--- voicespend/src/components/Main/List/listStyles.js
+++ voicespend/src/components/Main/List/listStyles.js
@@ -2,3 +2,5 @@ import { makeStyles } from '@material-ui/core/styles';
 import { red, green } from '@material-ui/core/colors';
 
 export default makeStyles((theme) => ({
+    avatarIncome: {
+        color: '#fff',
