43a66eda5ca3d1a51b865306f67194ddf4477919
arianeluteria <137739573+arianeluteria@users.noreply.github.com>
2024-01-22 18:28:30 -0800

    add
    Form.jsx

--- voicespend/src/components/Main/Form/Form.jsx
+++ voicespend/src/components/Main/Form/Form.jsx
@@ -2,3 +2,6 @@ import React from 'react';
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 import useStyles from './formStyles';
+
+const Form = () => {
+    const classes = useStyles();
