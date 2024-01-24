98a6d538727cdf94db125d9ba08b5e141286b232
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-25 00:49:21 +0800

    Update
    server.js

--- voicespend/src/server.js
+++ voicespend/src/server.js
@@ -3,13 +3,14 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
+const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(3001, () => {
-    console.log('Server is running on port 3001');
-  });
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -39,11 +40,12 @@ const connection = mysql.createConnection({
   });
 
   app.get('/transactions', (req, res) => {
-    db.query('SELECT * FROM transactions', (err, results) => {
+    connection.query('SELECT * FROM transactions', (err, results) => {
       if (err) {
-        res.status(500).json({ message: err.message });
+        console.error('Error fetching transactions: ', err);
+        res.status(500).json({ error: 'Error fetching transactions' });
       } else {
-        res.json(results);
+        res.status(200).json(results);
       }
     });
   });
