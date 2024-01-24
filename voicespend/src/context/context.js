afa3fc65c6cbaf7fe5a9b9f4a6151a077ffd1952
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-25 00:56:28 +0800

    update
    context.js

--- voicespend/src/context/context.js
+++ voicespend/src/context/context.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useReducer, createContext, useMemo } from 'react';
-
+import { useEffect, createContext, useReducer, useMemo } from 'react';
 import contextReducer from './contextReducer';
+import axios from 'axios';
 
 const initialState = [];
 
@@ -8,18 +8,6 @@ export const ExpenseTrackerContext = createContext(initialState);
 
 export const Provider = ({ children }) => {
   const [transactions, dispatch] = useReducer(contextReducer, initialState);
-
-  const deleteTransaction = (id) =>
-    dispatch({ type: 'DELETE_TRANSACTION', payload: id });
-  const addTransaction = (transaction) =>
-    dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
-  const clearTransactions = () =>
-    dispatch({ type: 'CLEAR_TRANSACTIONS' });
-
-  const balance = useMemo(() =>
-    transactions.reduce((acc, currVal) => currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount, 0),
-    [transactions]
-  );
 
   useEffect(() => {
     axios.get('http://localhost:5000/transactions') // Update with your backend URL
@@ -33,11 +21,35 @@ export const Provider = ({ children }) => {
       .catch((error) => console.error(error));
   };
 
+  const deleteTransaction = (id) => {
+    axios.delete(`http://localhost:5000/transactions/${id}`) // Update with your backend URL
+      .then(() => dispatch({ type: 'DELETE_TRANSACTION', payload: id }))
+      .catch((error) => console.error(error));
+  };
+
+  const updateTransaction = (updatedTransaction) => {
+    axios.put(`http://localhost:5000/transactions/${updatedTransaction.id}`, updatedTransaction) // Update with your backend URL
+      .then(() => dispatch({ type: 'UPDATE_TRANSACTION', payload: updatedTransaction }))
+      .catch((error) => console.error(error));
+  };
+
+  const clearTransactions = () => {
+    axios.delete('http://localhost:5000/transactions') // Update with your backend URL
+      .then(() => dispatch({ type: 'CLEAR_TRANSACTIONS' }))
+      .catch((error) => console.error(error));
+  };
+
+  const balance = useMemo(() =>
+    transactions.reduce((acc, currVal) => (currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount), 0),
+    [transactions]
+  );
+
   return (
     <ExpenseTrackerContext.Provider
       value={{
-        deleteTransaction,
         addTransaction,
+        deleteTransaction,
+        updateTransaction,
         clearTransactions,
         transactions,
         balance,
