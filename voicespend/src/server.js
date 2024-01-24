efe799ab3c5980e4f75670d1ce7e4a7f82dff383
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-24 23:52:53 +0800

    Update
    server.js

--- voicespend/src/server.js
+++ voicespend/src/server.js
@@ -6,3 +6,10 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Jkobdelapaz00',
+    database: 'expense_tracker',
+  });
