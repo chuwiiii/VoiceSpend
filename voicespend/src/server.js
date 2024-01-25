eda1b3929884bb8b7eaa3bb7621463a073afd647
Kurt Dela Paz <kurt.delapazzzz@gmail.com>
2024-01-25 14:53:10 +0800

    Update

--- voicespend/src/server.js
+++ voicespend/src/server.js
@@ -3,11 +3,6 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
